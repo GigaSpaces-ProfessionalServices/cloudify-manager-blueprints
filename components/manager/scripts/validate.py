e9ba76b66e946dffcd10e47a8ad05541517d464b
Isaac Shabtay <isaac@shabtay.com>
2016-02-28 22:30:21 -0800

    CFY-4901: validate definition of username and password

---
+++ components/manager/scripts/validate.py
@@ -0,0 +1,20 @@
+from cloudify import ctx
+from cloudify_cli import constants
+
+from cloudify.exceptions import NonRecoverableError
+import os
+
+try:
+    security_enabled = ctx.node.properties['security']['enabled']
+except KeyError:
+    security_enabled = False
+
+if security_enabled:
+    missing_keys = set([constants.CLOUDIFY_USERNAME_ENV,
+                        constants.CLOUDIFY_PASSWORD_ENV]) -\
+                   set(os.environ.keys())
+
+    if missing_keys:
+        raise NonRecoverableError(
+            'Security is enabled, but the following required environment '
+            'variables have not been set: {}'.format(list(missing_keys)))
